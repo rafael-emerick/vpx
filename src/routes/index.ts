36780898fdde2910cce654d1d7b8769fb9a7b3b8
rafael-emerick <rafaelemerick5@gmail.com>
2020-10-30 12:30:03 -0300

    Create routes.
    Criando as rotas para usuários, produtos, carrinho e pedidos. Alterando também o arquivo index das rotas para setar as rotas para os apontamentos corretos.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,21 @@
 import { Router } from 'express';
+import gendersRouter from './genders.routes';
+import paymentmethodsRouter from './paymentmethods.routes';
+import sizesRouter from './sizes.routes';
+import cartsRoutes from './carts.routes';
+import productsRoutes from './products.routes';
+import ordersRoutes from './orders.routes';
+import usersRoutes from './users.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello world!x' }));
+routes.use('/genders', gendersRouter);
+routes.use('/paymentmethods', paymentmethodsRouter);
+routes.use('/sizes', sizesRouter);
+routes.use('/carts', cartsRoutes);
+routes.use('/products', productsRoutes);
+routes.use('/orders', ordersRoutes);
+routes.use('/users', usersRouter);
 
 export default routes;
