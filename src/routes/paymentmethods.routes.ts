00ee22d6ea816f181ca35e46c5e0450b33fe1180
rafael-emerick <rafaelemerick5@gmail.com>
2020-10-30 11:55:37 -0300

    Create routes
    Rota para os valores estáticos de tamanho, método de pagamento e gênero.

---
+++ src/routes/paymentmethods.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const paymentmethodsRouter = Router();
+
+export default paymentmethodsRouter;
---
+++ src/routes/sizes.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const sizesRouter = Router();
+
+export default sizesRouter;
