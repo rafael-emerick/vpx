e4173dc15a692c51f77c674a0e0899b8f74c0626
rafael-emerick <rafaelemerick5@gmail.com>
2020-10-30 11:49:26 -0300

    Add json to server

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello world!' }));
+routes.get('/', (request, response) => response.json({ message: 'Hello world!x' }));
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import routes from './routes';
 
 const server = express();
 
+server.use(express.json());
+
 server.use(routes);
 
 server.listen(3333, () => {
